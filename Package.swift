83b83805ffb01bb3d00ec48df40a338e51e2b5c9
Swee Kwang <sweekwang94@gmail.com>
2023-04-20 20:15:31 +0800

    Update SwiftLint version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.50.4"),
-        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.51.7"),
+        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.51.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
